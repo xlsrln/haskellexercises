88b29386fdf15f689280d2f7891421332fa14305
xlsrln <31051075+xlsrln@users.noreply.github.com>
2017-10-25 17:54:35 +0200

    Update rövarspråket.hs

--- rövarspråket.hs
+++ rövarspråket.hs
@@ -6,9 +6,11 @@ rovarspraket         [] = []
 rovarspraket (x:resten) = 
         rovar x (head resten) ++ rovarspraket resten
 
+--needs more tweaking, e.g. for double consonants ck,tt, etc
+
 rovar x y =
     if elem x vowels
         then [x] ++ [y] ++ "o" -- ++ [y]
         else [x]
 
-vowels = "aeiyou"    
+vowels = "aeiyouåäö"    
